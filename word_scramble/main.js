27125d94bd70bf97e880eecf2dd20ed611def505
eghannoum <65394346+eghannoum@users.noreply.github.com>
2022-08-22 13:57:57 +0200

    Update main.js

--- word_scramble/main.js
+++ word_scramble/main.js
@@ -1,12 +1,48 @@
 let random_word = document.getElementById("random-word")
 let hint = document.getElementById("word-hint")
+let timer = document.getElementById("timer")
+let check_word = document.getElementById("check-word")
+let word = document.getElementById("word")
+let orginal_word = ''
+
 
 
 fetch('https://random-words-api.vercel.app/word').then(response => {
     return response.json().then(data => {
-        random_word.innerHTML = data[0].word
+        orginal_word = data[0].word.toLowerCase(orginal_word)
+        random_word.innerHTML = shuffle(orginal_word)
         hint.innerHTML = 'Hint: ' + data[0].definition
-        console.log(data[0])
 
     })
 });
+
+
+let timeleft = 1;
+let downloadTimer = setInterval(function () {
+    if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+    }
+    timer.innerHTML = `Time left: ${60 - timeleft}`;
+    timeleft += 1;
+}, 1000);
+
+
+function shuffle(string) {
+    var parts = string.split('');
+    for (var i = parts.length; i > 0;) {
+        var random = parseInt(Math.random() * i);
+        var temp = parts[--i];
+        parts[i] = parts[random];
+        parts[random] = temp;
+    }
+    return parts.join('');
+}
+
+function equal() {
+    console.log(orginal_word)
+    if (word.value === orginal_word) {
+        console.log('eqaul')
+    }
+}
+
+check_word.addEventListener('click', equal)
