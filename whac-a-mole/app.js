0c57dd2e78b12d4411b94872a5f282dccaef3903
G1thub123@@@ <eghannoum9@gmail.com>
2022-08-29 03:12:11 +0200

    update app.js

--- whac-a-mole/app.js
+++ whac-a-mole/app.js
@@ -4,7 +4,8 @@ const time = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let res = 0
-
+let courentTime = 60
+let selectedPos
 function randomSeq(){
     sqrs.forEach(sqr => {
         sqr.classList.remove('mole')
@@ -12,7 +13,33 @@ function randomSeq(){
 
     let randSeq = sqrs[Math.floor(Math.random()*9)]
     randSeq.classList.add('mole')
+
+    selectedPos = randSeq.id
 }
 
+sqrs.forEach(seq => {
 
-randomSeq()
+    seq.addEventListener('mousedown', () => {
+        if (seq.id == selectedPos){
+            res++
+            score.innerHTML = res
+        }
+        selectedPos = null
+        randomSeq()
+    })
+})
+
+
+function countDown(){
+    courentTime--
+    time.innerHTML = courentTime
+    if(courentTime == 0){
+        clearInterval(countDownTimer)
+        sqrs.forEach(seq => {
+            seq.removeEventListener('mousedown')
+        })
+
+    }
+}
+
+let countDownTimer = setInterval(countDown, 1000)
